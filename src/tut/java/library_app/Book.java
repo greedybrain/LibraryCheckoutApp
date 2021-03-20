ef60e653d897332d78a952c969ea57be7780c3c8
greedybrain <greedrybrain18@gmail.com>
2021-03-19 21:56:16 -0400

    added summary to state

--- src/tut/java/library_app/Book.java
+++ src/tut/java/library_app/Book.java
@@ -4,15 +4,19 @@ public class Book {
     private String title;
     private String author;
     private String ISBN;
+    private String summary;
 
-    public Book(String title, String author, String ISBN) {
+    public Book(String title, String author, String ISBN, String summary) {
         this.title = title;
         this.author = author;
         this.ISBN = ISBN;
+        this.summary = summary;
+
     }
 
     // getters below
     public String getTitle() { return title; }
+    public String getSummary() { return summary; }
     public String getAuthor() { return author; }
     public String getISBN() { return ISBN; }
 }
