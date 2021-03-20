37874fbe31a1a6ee863aed2fe555aed9983b3617
greedybrain <greedrybrain18@gmail.com>
2021-03-19 22:41:30 -0400

    added display more book details method

--- src/tut/java/library_app/Librarian.java
+++ src/tut/java/library_app/Librarian.java
@@ -9,8 +9,9 @@ public class Librarian extends Person {
 
     public Librarian(String fullName) { super(fullName); }
 
-    public void handleCheckoutFor(User user, Librarian librarian) {
+    public void handleCheckoutFor(User user) {
         Scanner scanner1 = new Scanner(System.in);
+
         while(true) {
             Inventory.showOptions();
             var bookNumber = scanner1.nextInt();
@@ -18,6 +19,8 @@ public class Librarian extends Person {
                 System.out.println("Enter a valid choice number\n");
                 continue;
             }
+
+            displayMoreBookDetails();
 
             if (user.getCheckouts().contains(books.get(bookNumber - 1))) {
                 System.out.println("You already checked that book out, try another");
@@ -29,18 +32,44 @@ public class Librarian extends Person {
             }
             if (canCheckoutNewBook(user)) continue;
             else {
-                var receipt = new Receipt();
-                receipt.displayOfTransactionInfo(user, librarian);
+
                 break;
             }
         }
     }
 
-    private static boolean canCheckoutNewBook(User user) {
+    private static void displayMoreBookDetails() {
         Scanner scanner2 = new Scanner(System.in);
+        Scanner scanner3 = new Scanner(System.in);
+
+        while(true) {
+            System.out.print("Enter the number of the book you wish to see more details about: ");
+            var selection = scanner2.nextInt();
+            if (selection < 1 || selection > 10) {
+                System.out.println("Enter a valid choice number\n");
+                continue;
+            } else {
+                var book = books.get(--selection);
+                System.out.printf("Title: %s%n", book.getTitle());
+                System.out.printf("Author: %s%n", book.getAuthor());
+                System.out.printf("ISBN: %s%n", book.getISBN());
+                System.out.printf("Summary%n");
+                System.out.println("==============");
+                System.out.printf("%s%n", book.getSummary());
+                System.out.println();
+                System.out.println("View details about another? (y/n): ");
+                var decision = scanner3.nextLine().toLowerCase(Locale.ROOT);
+                if (decision.equals("y")) continue;
+                else break;
+            }
+        }
+    }
+
+    private static boolean canCheckoutNewBook(User user) {
+        Scanner scanner = new Scanner(System.in);
         if (user.getCheckouts().size() != 5) {
             System.out.printf("Add another book? (y/n): %n");
-            var decision = scanner2.nextLine().toLowerCase(Locale.ROOT);
+            var decision = scanner.nextLine().toLowerCase(Locale.ROOT);
             return decision.equals("y");
         } else {
             System.out.println();
