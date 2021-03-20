8a17d70ef09ad66326ad5882847f464ddfd147a9
greedybrain <greedrybrain18@gmail.com>
2021-03-20 17:42:41 -0400

    minor variable renaming to reduce lines of code

--- src/tut/java/library_app/Librarian.java
+++ src/tut/java/library_app/Librarian.java
@@ -22,29 +22,26 @@ public class Librarian extends Person {
 
             displayMoreBookDetails();
 
-            if (user.getCheckouts().contains(books.get(bookNumber - 1))) {
+            var userCheckouts = user.getCheckouts();
+            if (userCheckouts.contains(books.get(bookNumber - 1))) {
                 System.out.println("You already checked that book out, try another");
                 continue;
             }
             else {
-                user.getCheckouts().add(books.get(--bookNumber));
-                System.out.println(user.getCheckouts());
+                userCheckouts.add(books.get(--bookNumber));
+                System.out.println(userCheckouts);
             }
-            if (canCheckoutNewBook(user)) continue;
-            else {
-
-                break;
-            }
+            if (!canCheckoutNewBook(user)) break;
         }
     }
 
     private static void displayMoreBookDetails() {
+        Scanner scanner1 = new Scanner(System.in);
         Scanner scanner2 = new Scanner(System.in);
-        Scanner scanner3 = new Scanner(System.in);
 
         while(true) {
             System.out.print("Enter the number of the book you wish to see more details about: ");
-            var selection = scanner2.nextInt();
+            var selection = scanner1.nextInt();
             if (selection < 1 || selection > 10) {
                 System.out.println("Enter a valid choice number\n");
                 continue;
@@ -58,7 +55,7 @@ public class Librarian extends Person {
                 System.out.printf("%s%n", book.getSummary());
                 System.out.println();
                 System.out.println("View details about another? (y/n): ");
-                var decision = scanner3.nextLine().toLowerCase(Locale.ROOT);
+                var decision = scanner2.nextLine().toLowerCase(Locale.ROOT);
                 if (decision.equals("y")) continue;
                 else break;
             }
@@ -66,16 +63,15 @@ public class Librarian extends Person {
     }
 
     private static boolean canCheckoutNewBook(User user) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner scanner1 = new Scanner(System.in);
         if (user.getCheckouts().size() != 5) {
             System.out.printf("Add another book? (y/n): %n");
-            var decision = scanner.nextLine().toLowerCase(Locale.ROOT);
+            var decision = scanner1.nextLine().toLowerCase(Locale.ROOT);
             return decision.equals("y");
-        } else {
-            System.out.println();
-            System.out.println("You've reached your limit for checkouts. Please check a book in to check out another.");
-            System.out.println();
         }
+        System.out.println();
+        System.out.println("You've reached your limit for checkouts. Please check a book in to check out another.");
+        System.out.println();
         return false;
     }
 }
