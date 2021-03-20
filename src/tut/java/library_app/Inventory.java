b5db50de4f134ef232a5083ec4b597d8e7589e77
greedybrain <greedrybrain18@gmail.com>
2021-03-19 22:40:43 -0400

    added summary to to constructor when creating an instance

--- src/tut/java/library_app/Inventory.java
+++ src/tut/java/library_app/Inventory.java
@@ -8,16 +8,16 @@ public class Inventory {
 
     // todo: init books / get all books
     public static List<Book> all() {
-        BOOKS.add(new Book("To Kill a Mockingbird", "Harper Lee", "1111"));
-        BOOKS.add(new Book("1984", "George Orwell", "2222"));
-        BOOKS.add(new Book("Harry Potter and the Philosopher’s Stone", "J.K. Rowling", "3333"));
-        BOOKS.add(new Book("The Lord of the Rings", "J.R.R. Tolkien", "4444"));
-        BOOKS.add(new Book("The Great Gatsby", "Scott Fitzgerald", "5555"));
-        BOOKS.add(new Book("Pride and Prejudice", "Jane Austen", "6666"));
-        BOOKS.add(new Book("The Diary Of A Young Girl", "Ann Frank", "7777"));
-        BOOKS.add(new Book("The Book Thief", "Markus Zusak", "8888"));
-        BOOKS.add(new Book("The Hobbit", "J.R.R. Tolkien", "9999"));
-        BOOKS.add(new Book("Little Women", "Louisa May Alcott", "0000"));
+        BOOKS.add(new Book("To Kill a Mockingbird", "Harper Lee", "1111", "Published in 1960, this timeless classic explores human behaviour and the collective conscience of The Deep South in the early 20th century. Humour entwines the delicate strands of prejudice, hatred, hypocrisy, love and innocence to create one of the best novels ever written."));
+        BOOKS.add(new Book("1984", "George Orwell", "2222", "Although 1984 has passed us by, George Orwell’s dystopian, totalitarian world of control, fear and lies has never been more relevant. Delve into the life of Winston Smith as he struggles with his developing human nature in a world where individuality, freewill and love are forbidden."));
+        BOOKS.add(new Book("Harry Potter and the Philosopher’s Stone", "J.K. Rowling", "3333", "I’m willing to bet you’ve heard of Harry Potter, but have you read the books? Join Harry Potter as he begins his journey into the world of magic, where he is the celebrated Boy Who Lived. Visit Hogwarts, meet your favourite characters and watch Harry grow into the one of the most famous literary characters in the world."));
+        BOOKS.add(new Book("The Lord of the Rings", "J.R.R. Tolkien", "4444", "Middle Earth is a wonderful, expansive fantasy world filled with turmoil, heroes, evil and innocence. Although our protagonist Frodo Baggins’ quest seems impossible to complete, this trilogy is a tale of triumph in the most impossible circumstances."));
+        BOOKS.add(new Book("The Great Gatsby", "Scott Fitzgerald", "5555", "Published in 1925, Fitzgerald’s The Great Gatsby explores the decadence of the Jazz Age, and one man’s introduction into a world where even those with the most indulgent lives cannot earn love."));
+        BOOKS.add(new Book("Pride and Prejudice", "Jane Austen", "6666", "One of the most famous novels of all time, Pride And Prejudice details the courtship of two opposed characters in a world where manners and courtesy are of the utmost importance."));
+        BOOKS.add(new Book("The Diary Of A Young Girl", "Ann Frank", "7777", "Unforgettable and deeply influential, Anne Frank’s diary is a raw account of a young girl’s life as she hides from the Nazis. Despite her circumstances, Anne believes that people are still good at heart and that the world is full of beauty: she will change your life."));
+        BOOKS.add(new Book("The Book Thief", "Markus Zusak", "8888", "Set in Germany during 1939, The Book Thief follows Liesel as she rescues books from the tyranny of Nazi rule. Meanwhile, her family has hidden a Jewish fighter in their basement and death looks down on the family, narrating our tale. Experience bravery that is rarely found in the world, and friendship that is formed in the most unlikely of situations."));
+        BOOKS.add(new Book("The Hobbit", "J.R.R. Tolkien", "9999", "Although the movies are inexplicably long, The Hobbit was originally written as a short children’s book. Meet your favourite characters for the first time as the unforgettable Bilbo Baggins traverses the harsh landscapes of Middle Earth to challenge a dragon."));
+        BOOKS.add(new Book("Little Women", "Louisa May Alcott", "0000", "Join four sisters, each with their own prominent personality, as they come of age in charming 19th Century New England. Experience their struggles and revel in their flaws, as these girls become strong women."));
 
         return BOOKS;
     }
